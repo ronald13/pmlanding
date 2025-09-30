836f64f093663066170500e9c8b66ca51b4edcf4
Aliaksandr Ihantsiuk <a.ihnatsiuk@digital-science.com>
2025-09-30 11:20:22 +0200

    chore: added configuration

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,5 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
+// gatsby-node.js
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-}
+  const { createPage } = actions;
+  // Здесь будут создаваться динамические страницы если понадобится
+};
